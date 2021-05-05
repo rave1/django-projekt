1aea76cc3b87ee4e7d7882100f1ab3b3bfe7f685
Jakub Sopel <jakubsopel7@gmail.com>
2021-05-05 16:21:32 +0200

    podstawowy setup admina i obiektów

--- leads/models.py
+++ leads/models.py
@@ -9,29 +9,33 @@ class User(AbstractUser): #dziedzicze z wbudowanego modelu uzytkownika w django
     pass
 
 class Lead(models.Model):
-'''
+    '''
     SOURCE_CHOICES = (
         ('YouTube', 'YouTube'),
         ('Google','Google'),
         ('Newsletter', 'Newsletter'),
     )
-'''
+    '''
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    agent = models.ForeignKey("Agent" on_delete=models.CASCADE) # przy usunieciu agenta, Lead tez sie usuwa
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE) # przy usunieciu agenta, Lead tez sie usuwa
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # OneToOne -> kazdy agent ma jednego uzytkownika
-
+    def __str__(self):
+        return self.user.email # zwracanie stringa gdy jest używany menadżer modeli
 
        
 
-'''
+    '''
     phoned = models.BooleanField(default=False)
     source  = models.CharField(choices=SOURCE_CHOICES, max_length=100)
 
     profile_picture = models.ImageField(blank=True, null=True) ##blank -> pusty string
     special_files = models.FileField(blank=True, null=True)
-'''
+    '''
